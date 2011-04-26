9e1cbf5ab6ca049902918731a9ddc30275d8721d
Thomas Nixon <tom@tomn.co.uk>
2011-04-27 00:08:05 +0100

    Updated the demo to work with the new pyscad interface.

--- script_demo.py
+++ script_demo.py
@@ -2,6 +2,8 @@ from pyscad_script import Model
 from pyscad import *
 
 # The original way (provided by pyscad).
+# This doesn't work, as there is no pyscad.scale or pyscad.translate, but you
+# get the idea.
 def generate_old():
 	cube1 = translate([20, 0, 0], [scale([2, 1, 1], [cube(10)])])
 	cube2 = cube(10)
@@ -34,7 +36,7 @@ def generate_with_my_operator():
 	model = Model()
 	
 	with model.push(my_intersect, 5):
-		model.sphere(3, position=(10,10,10))
+		model.sphere(3, center=(10,10,10))
 		model.cube(10)
 	
 	print "### generate_with_my_operator"
@@ -42,6 +44,5 @@ def generate_with_my_operator():
 
 
 if __name__ == "__main__":
-	generate_old()
 	generate_new()
 	generate_with_my_operator()
